98e89354e07c5b4220aa4759217ae886bdff9437
Prajwal Pandey <prajwal.pandey@tensech.com>
2022-12-16 01:26:03 +0530

    created a simple form with html and send a req via post method

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const http = require('http');
 // }
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if(url === '/'){
+    res.write('<html>');
+    res.write('<head> <title>Message from server</title></head>')
+    res.write('<body> <form action="/message" method=""POST> <input type=text><button type="submit">Send</button> </form> </body>')
+    return res.end();
+}
     res.setHeader('Content-type', 'text/html');
     res.write('<html> <head> <title>Hello there</title></head> <body> <h1>Hello there, from nodejs server</h1> </body> </html>');
     res.end();
