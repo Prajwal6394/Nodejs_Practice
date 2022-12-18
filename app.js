ff251d0270877bc32cc8ad691dbcb2f15dfcad4e
Prajwal Pandey <prajwal.pandey@tensech.com>
2022-12-19 01:35:06 +0530

    handleing error in nodejs and synchronous and asynchronous methods for file uploading

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const server = http.createServer((req, res) => {
         res.write('<body> <form action="/message" method="POST"> <input name = "message" type=text><button type="submit">Send</button> </form> </body>')
         return res.end();
     }
-    if(url === '/message' && method === "POST"){
+    if (url === '/message' && method === "POST") {
         const body = [];
         req.on('data', (chucks) => {
             console.log(chucks);
@@ -23,12 +23,12 @@ const server = http.createServer((req, res) => {
             const parseBody = Buffer.concat(body).toString();
             console.log(parseBody);
             const messageToBeloadedInTxtFile = parseBody.split('=')[1];
-            fs.writeFileSync('message.txt', messageToBeloadedInTxtFile);
+            fs.writeFile('message.txt', messageToBeloadedInTxtFile, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
-        
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
     res.setHeader('Content-type', 'text/html');
     res.write('<html> <head> <title>Hello there</title></head> <body> <h1>Hello there, from nodejs server</h1> </body> </html>');
