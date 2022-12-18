425ec2f4e11ffb6c801e4e724f01f6fbb734e2af
Prajwal Pandey <prajwal.pandey@tensech.com>
2022-12-19 01:51:33 +0530

    module system in nodejs, separating file to make our code base more readable and easy to understand

--- app.js
+++ app.js
@@ -1,37 +1,8 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./route');
 // function rqListner(req, res){
 
 // }
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head> <title>Message from server</title></head>')
-        res.write('<body> <form action="/message" method="POST"> <input name = "message" type=text><button type="submit">Send</button> </form> </body>')
-        return res.end();
-    }
-    if (url === '/message' && method === "POST") {
-        const body = [];
-        req.on('data', (chucks) => {
-            console.log(chucks);
-            body.push(chucks);
-        });
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            console.log(parseBody);
-            const messageToBeloadedInTxtFile = parseBody.split('=')[1];
-            fs.writeFile('message.txt', messageToBeloadedInTxtFile, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-    res.setHeader('Content-type', 'text/html');
-    res.write('<html> <head> <title>Hello there</title></head> <body> <h1>Hello there, from nodejs server</h1> </body> </html>');
-    res.end();
-})
+const server = http.createServer(routes);
 server.listen(3000);
