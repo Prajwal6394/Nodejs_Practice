e55550a7a8a183923b946c9da9fdab8940ffbf1b
Prajwal Pandey <prajwal.pandey@tensech.com>
2023-02-21 00:27:56 +0530

    started working and understanding middle ware

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const http = require('http');
 
 const express = require('express');
 const app = express();
-// function rqListner(req, res){
 
-// }
+
+app.use((req, res, next) => {
+    console.log("In the middleware!");
+    // This next function allow this first middle ware to go to the next middle ware for execution
+    next();
+})
+app.use((req, res, next) => {
+    console.log("This is another middleware");
+})
 
 const server = http.createServer(app);
 server.listen(3000);
