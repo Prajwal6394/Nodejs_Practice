d7bd01c10f560760f6312e5b61b72840460ac4d8
Prajwal Pandey <prajwal.pandey@tensech.com>
2023-02-22 01:15:40 +0530

    building 404 page not found logic

--- app.js
+++ app.js
@@ -12,5 +12,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(adminRotes);
 app.use(shop);
 
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+})
+
 
 app.listen(3000);
